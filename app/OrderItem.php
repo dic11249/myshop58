285b38ae4827afbb9dcdf3a8afcb19505947ce65
BMO <dic11249@gmail.com>
2021-06-11 15:18:09 +0800

    修改訂單及訂單項目的關聯

--- app/OrderItem.php
+++ app/OrderItem.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo('App\Order');
+    }
 }
